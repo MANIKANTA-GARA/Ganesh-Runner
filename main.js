562bd4c219af31296802379c78440f96c6cd31f4
Gara Manikanta <manikantagara1234@gmail.com>
2026-09-17 21:48:45 +0530

    Sync: main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+﻿// main.js - Application Entry Point
+import { Game } from './src/core/Game.js';
+import { UIManager } from './src/ui/UIManager.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const canvasContainer = document.getElementById('game-canvas-container');
+  const ui = new UIManager();
+  const game = new Game(canvasContainer, ui);
+
+  ui.setGame(game);
+  ui.checkInitialScreen();
+});
